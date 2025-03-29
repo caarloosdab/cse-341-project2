1e8a6ab5f03a9539ff758ece78adccfd7dfc8200
caarloosdab <caarloosdab@gmail.com>
2025-03-29 16:47:50 -0600

    appi finished

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'users Api',
+        description: 'users Api',
+    },
+    host: 'localhost:3000',
+    schemes: ['http', 'https'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js']
+
+// this will generate the swagger.json file
+swaggerAutogen(outputFile, endpointsFiles, doc);
